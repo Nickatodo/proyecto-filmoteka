d6fe8ceb51709170a24d6d7ea8e3da2acfef15d1
Nicolas Canales Diaz <nicolasjcanalesdiaz@gmail.com>
2024-03-09 18:05:11 -0300

    Correcion de modales
    Ahora funcionan bien los modales.

--- src/js/guardar_pelicula.js
+++ src/js/guardar_pelicula.js
@@ -3,12 +3,14 @@ const watchedButtons = document.querySelectorAll('.watched-button');
 
 // Se le agrega a cada boton el event listener
 watchedButtons.forEach(button => { 
-    button.addEventListener('click', function () {
-        const movieTitle = document.querySelector("h2").innerText;
-        markAsWatched(movieTitle); // Llama a markAsWatched con el titulo de la pelicula
+  button.addEventListener('click', function () {
+    const movieId = document.querySelector(".information__id").innerText;
+    const movieTitle = document.querySelector(".information__title").innerText;  
+    const movieImg = document.querySelector(".movie_img").src;
+        markAsWatched(); // Llama a markAsWatched con el titulo de la pelicula
         this.disabled = true; //Deshabilita el boton para evitar mas clicks
         this.classList.add('clicked');
-        marcarComoFavorita(movieTitle);
+        marcarComoFavorita(movieId,movieTitle,movieImg);
     });
 });
 
@@ -23,11 +25,15 @@ const addToQueueButtons = document.querySelectorAll('.addToQueue-button');
 
 // Se le agrega a cada boton el event listener
 addToQueueButtons.forEach(button => {
-    button.addEventListener('click', function () {
-        const movieTitle = document.querySelector("h2").innerText;
-        addToQueue(movieTitle); // Llama a markAsWatched con el titulo de la pelicula
-        this.disabled = true; //Deshabilita el boton para evitar mas clicks
-        this.classList.add('clicked');
+  button.addEventListener('click', function () {
+    const movieId = document.querySelector(".information__id").innerText;
+    const movieTitle = document.querySelector(".information__title").innerText;
+    const movieImg = document.querySelector(".movie_img").innerText;
+
+      
+      addToQueue(movieTitle); // Llama a markAsWatched con el titulo de la pelicula
+      this.disabled = true; //Deshabilita el boton para evitar mas clicks
+      this.classList.add('clicked');
     });
 });
 
@@ -37,16 +43,18 @@ function addToQueue(title) {
   // Aqui entonces envía al server como Watched
 }
 
-function marcarComoFavorita(id) {
+function marcarComoFavorita(id,title,img) {
   let favoritas = JSON.parse(localStorage.getItem('peliculasFavoritas')) || [];
 
   // Verificar si la película ya está marcada como favorita
-  // if (favoritas.includes(id)) {
-  //   alert('Esta película ya está marcada como favorita.');
-  //   return;
-  // }
+  if (favoritas.includes(id)) {
+     alert('Esta película ya está marcada como favorita.');
+     return;
+   }
 
-  favoritas.push(id);
+  dattos = [id, title, img];
+  console.log(dattos);
+  favoritas.push(dattos);
   localStorage.setItem('peliculasFavoritas', JSON.stringify(favoritas));
   // alert('Película marcada como favorita.');
 }
--- src/js/modal_pelicula.js
+++ src/js/modal_pelicula.js
@@ -1,25 +1,25 @@
-(() => {
+
+export function modales() {
     
-    setTimeout(() => {
-        const refs = {
-            openModalBtn: document.querySelectorAll("[data-modal-open]"),
-            closeModalBtn: document.querySelector("[data-modal-close]"),
-            modal: document.querySelector("[data-modal]"),
-        };
-        
-        for (let index = 0; index < refs.openModalBtn.length; index++){
-            
-            refs.openModalBtn[index].addEventListener("click", toggleModal);
-        }   
-        
-        refs.closeModalBtn.addEventListener("click", toggleModal);
+    const refs = {
+        openModalBtn: document.querySelectorAll("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
 
-        function toggleModal() {
-            refs.modal.classList.toggle("is-hidden");
-        }
-        
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
 
-    }, 250);
+    function toggleCerrar() { 
+        refs.modal.classList.add("is-hidden");
+    }
+    
+    for (let index = 0; index < refs.openModalBtn.length; index++){
+        refs.openModalBtn[index].addEventListener("click", toggleModal);
+    }   
 
+    refs.closeModalBtn.addEventListener("click",toggleCerrar);
 
-})();
+}
+
