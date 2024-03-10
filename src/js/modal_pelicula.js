a77f4b8e0b9638f9450fc4d1c21ca69bbfa51f49
Nicolas Canales Diaz <nicolasjcanalesdiaz@gmail.com>
2024-03-10 15:51:56 -0300

    Correcion modal

--- src/js/modal_pelicula.js
+++ src/js/modal_pelicula.js
@@ -1,38 +1,33 @@
 
 export function modales() {
     
-        const refs = {
-            openModalBtn: document.querySelectorAll("[data-modal-open]"),
-            closeModalBtn: document.querySelector("[data-modal-close]"),
-            modal: document.querySelector("[data-modal]"),
-        };
+    const refs = {
+        openModalBtn: document.querySelectorAll("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+    
+    for (let index = 0; index < refs.openModalBtn.length; index++){
         
-        for (let index = 0; index < refs.openModalBtn.length; index++){
-            
-            refs.openModalBtn[index].addEventListener("click", toggleModal);
-            refs.openModalBtn[index].addEventListener("click",startAnimation);
-        }   
-        
-        refs.closeModalBtn.addEventListener("click", toggleModal);
-        refs.closeModalBtn.addEventListener("click", removeAnimation);
-        
-        function startAnimation(){
-            refs.modal.classList.add("animate__zoomIn")
-        }
-        function toggleModal() {
-            refs.modal.classList.toggle("is-hidden");
-        }
-        function removeAnimation(){
-            refs.modal.classList.remove("animate__zoomIn");
-        }
-
-/*
+        refs.openModalBtn[index].addEventListener("click", toggleModal);
+        refs.openModalBtn[index].addEventListener("click",startAnimation);
+    }   
+    
+    refs.closeModalBtn.addEventListener("click", toggleCerrar);
+    refs.closeModalBtn.addEventListener("click", removeAnimation);
+    
+    function startAnimation(){
+        refs.modal.classList.add("animate__zoomIn")
+    }
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+    function removeAnimation(){
+        refs.modal.classList.remove("animate__zoomIn");
+    }
     function toggleCerrar() { 
         refs.modal.classList.add("is-hidden");
     }
     
-
-    refs.closeModalBtn.addEventListener("click",toggleCerrar);
-*/
 }
 
