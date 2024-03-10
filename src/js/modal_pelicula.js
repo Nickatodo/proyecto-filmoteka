8809aa4db28b5bd876155a3be107bb7ba21eaf71
NCameloG <dev.ncamelog@gmail.com>
2024-03-09 23:09:42 -0500

    se agrega animaciona a los modales y se modifican estilos

--- src/js/modal_pelicula.js
+++ src/js/modal_pelicula.js
@@ -10,14 +10,21 @@
         for (let index = 0; index < refs.openModalBtn.length; index++){
             
             refs.openModalBtn[index].addEventListener("click", toggleModal);
+            refs.openModalBtn[index].addEventListener("click",startAnimation);
         }   
         
         refs.closeModalBtn.addEventListener("click", toggleModal);
-
+        refs.closeModalBtn.addEventListener("click", removeAnimation);
+        
+        function startAnimation(){
+            refs.modal.classList.add("animate__zoomIn")
+        }
         function toggleModal() {
             refs.modal.classList.toggle("is-hidden");
         }
-        
+        function removeAnimation(){
+            refs.modal.classList.remove("animate__zoomIn");
+        }
 
     }, 250);
 
